ca0cbc0267412ecb8575093a6bdd609880c72ac8
Spencer Miskoviak <smiskoviak@gmail.com>
2017-01-01 16:54:03 -0600

    convert to postgres and add more user data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  first_name             :string           not null
+#  last_name              :string           not null
+#  slack_username         :string
+#  facebook_username      :string
+#  twitter_username       :string
+#  github_username        :string
+#  profile_image_url      :string
+#  position_name          :string
+#  member_since           :datetime         not null
+#  is_admin               :boolean          default(FALSE), not null
+#
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
---
+++ db/migrate/20170101223246_add_user_data.rb
@@ -0,0 +1,14 @@
+class AddUserData < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :slack_username, :string, null: true
+    add_column :users, :facebook_username, :string, null: true
+    add_column :users, :twitter_username, :string, null: true
+    add_column :users, :github_username, :string, null: true
+    add_column :users, :profile_image_url, :string, null: true
+    add_column :users, :position_name, :string, null: true
+    add_column :users, :member_since, :datetime, null: false
+    add_column :users, :is_admin, :boolean, null: false, default: false
+  end
+end
