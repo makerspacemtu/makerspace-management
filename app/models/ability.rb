e38b88b208d2ef42972eb52e2f89141e805713dc
jjwyrzyk <jjwyrzyk@mtu.edu>
2018-11-01 11:45:37 -0400

    Feature additions and bug fixes
    This adds a checkout all button, a back button at the trainings menu, and a framework to start setting up shift scheduling.  Also, trainings are now shown for each coach in the checkin menu.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -38,6 +38,12 @@ class Ability
       can :update, DailyReport
       can :create, DailyReport
       can :destroy, DailyReport
+      # admins can edit and see their own shifts
+      can :read, Signup
+      can :new, Signup
+      can :destroy, Signup
+      can :create, Signup
+
     elsif user.staff?
       # staff can see any user
       can :read, User
@@ -57,6 +63,8 @@ class Ability
       # staff can edit their own daily reports
       can :edit, DailyReport, user_id: user.id
       can :update, DailyReport, user_id: user.id
+      # staff can edit and see their own shifts
+
     end
     # members cannot currently do anything
   end
