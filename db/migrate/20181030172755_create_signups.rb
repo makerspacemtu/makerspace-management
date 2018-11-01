16e1f7b13cfaf89af6f2952385fef748e0285199
jjwyrzyk <jjwyrzyk@mtu.edu>
2018-11-01 14:39:00 -0400

    Revert "Revert "Feature additions and bug fixes""
    This reverts commit 89e7b2722705863624ad7e985a48c726f5e6e118.

---
+++ db/migrate/20181030172755_create_signups.rb
@@ -0,0 +1,23 @@
+class CreateSignups < ActiveRecord::Migration[5.0]
+  def change
+    create_table :signups do |t|
+      t.string :name, null: false
+      t.references :user, null: false
+      t.string :signup_day, null: false
+      t.string :signup_time, null:false
+      t.timestamps
+    end
+
+    # the many to many relationship for users and signups
+    create_table :user_signups do |t|
+      t.belongs_to :user, index: true, null: false
+      t.belongs_to :signup, index: true, null: false
+      t.timestamps
+    end
+
+    # to record who adds their signup
+    add_reference :user_signups, :created_by, references: :users, index: true, null: false
+    add_foreign_key :user_signups, :users, column: :created_by_id
+    add_index :user_signups, [:user_id, :signup_time], unique: true
+    end
+  end
