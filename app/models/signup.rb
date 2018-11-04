03461c0dae10881556973bc631fcf7fc4ab8b633
jjwyrzyk <jjwyrzyk@mtu.edu>
2018-11-04 15:54:38 -0500

    Furthers framework for signups, and adds databases addin

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -13,48 +13,49 @@
 class Signup < ApplicationRecord
 
   SIGNUP_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  SIGNUP_TIMES = ['3-4pm', '4-5pm', '5-6pm', '7-8pm', '8-9pm']
+  #SIGNUP_TIMES = ['3-4pm', '4-5pm', '5-6pm', '7-8pm', '8-9pm']
+  #SIGNUP_QTYS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
   has_many :user_signups
   has_many :users, :through => :user_signups
 
-  validates :name, presence: true
-  validates :signup_day, inclusion: { in: SIGNUP_DAYS }
-  validates :signup_time, inclusion: { in: SIGNUP_TIMES }
+  # validates :signup_id, presence: true
+  # validates :signup_day , inclusion: SIGNUP_DAYS
+  # validates :signup_time, inclusion: SIGNUP_TIMES
 
   def signup_day_format
     case self.signup_day
-    when "Monday"
-      1
-    when "Tuesday"
-      2
-    when "Wednesday"
-      3
-    when "Thursday"
-      4
-    when "Friday"
-      5
-    when "Saturday"
-      6
-    when "Sunday"
-      7
+    when 0
+      "Monday"
+    when 1
+      "Tuesday"
+    when 2
+      "Wednesday"
+    when 3
+      "Thursday"
+    when 4
+      "Friday"
+    when 5
+      "Saturday"
+    when 6
+      "Sunday"
     end
   end
 
   def signup_time_format
     case self.signup_time
-    when "3-4pm"
-      15
-    when "4-5pm"
-      16
-    when "5-6pm"
-      17
-    when "6-7pm"
-      18
-    when "7-8pm"
-      19
-    when "8-9pm"
-      20
+    when 15
+      "3-4pm"
+    when 16
+      "4-5pm"
+    when 17
+      "5-6pm"
+    when 18
+      "6-7pm"
+    when 19
+      "7-8pm"
+    when 20
+      "8-9pm"
     end
   end
-end
+ end
