87c2ea5ef92a46ed393971d2088b383eab48e05a
jjwyrzyk <jjwyrzyk@mtu.edu>
2018-12-29 14:32:32 -0500

    Have signup quanities preselection

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -13,7 +13,8 @@
 class Signup < ApplicationRecord
 
   SIGNUP_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  SIGNUP_TIMES = ['3-4pm', '4-5pm', '5-6pm', '7-8pm', '8-9pm']
+  #SIGNUP_TIMES = ['3-4pm', '4-5pm', '5-6pm', '7-8pm', '8-9pm']
+  SIGNUP_QTYS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
   has_many :user_signups
   has_many :users, :through => :user_signups
