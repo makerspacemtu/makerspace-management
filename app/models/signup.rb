a744940b6ee4de5fccd8955a9b8f3f8403fe0020
jjwyrzyk <jjwyrzyk@mtu.edu>
2019-01-01 17:06:05 -0500

    Signup model aligns with new db file

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -21,41 +21,5 @@ class Signup < ApplicationRecord
 
   validates :name, presence: true
   validates :signup_day, inclusion: { in: SIGNUP_DAYS }
-  validates :signup_time, inclusion: { in: SIGNUP_TIMES }
-
-  def signup_day_format
-    case self.signup_day
-    when "Monday"
-      1
-    when "Tuesday"
-      2
-    when "Wednesday"
-      3
-    when "Thursday"
-      4
-    when "Friday"
-      5
-    when "Saturday"
-      6
-    when "Sunday"
-      7
-    end
-  end
-
-  def signup_time_format
-    case self.signup_time
-    when "3-4pm"
-      15
-    when "4-5pm"
-      16
-    when "5-6pm"
-      17
-    when "6-7pm"
-      18
-    when "7-8pm"
-      19
-    when "8-9pm"
-      20
-    end
-  end
+  
 end
