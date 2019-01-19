bf1fd6e5398b3541110c24df0d0787ccb93db5e4
jjwyrzyk <jjwyrzyk@mtu.edu>
2019-01-19 15:32:07 -0500

    Add card styling depending on whether coach agreements have been completed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @makeragree = @user.trainings.exists?(name: 'Maker Coach Agreement')
 
     if @user.slack_user_id.present? && valid_slack_client?
       @slack_username = slack_client.users_info(user: @user.slack_user_id)[:user][:name]
