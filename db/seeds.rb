08beab3b2e26483b408d720e3a578ae0d20e8012
jjwyrzyk <jjwyrzyk@mtu.edu>
2019-01-22 17:07:56 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,32 @@
-admin = User.where(
-  email: "admin@mtu.edu"
-).first_or_create(
-  password: "password",
-  first_name: "Bob",
-  last_name: "Smith",
-  member_since: Time.now,
-  user_type: "Admin"
-)
-
-staff = User.where(
-  email: "staff@mtu.edu"
-).first_or_create(
-  password: "password",
-  first_name: "Jill",
-  last_name: "Doe",
-  member_since: Time.now,
-  user_type: "Staff"
-)
-
-member = User.where(
-  email: "member@mtu.edu"
-).first_or_create(
-  password: "password",
-  first_name: "Jack",
-  last_name: "William",
-  member_since: Time.now,
-  user_type: "Member"
-)
+# admin = User.where(
+#   email: "admin@mtu.edu"
+# ).first_or_create(
+#   password: "password",
+#   first_name: "Bob",
+#   last_name: "Smith",
+#   member_since: Time.now,
+#   user_type: "Admin"
+# )
+#
+# staff = User.where(
+#   email: "staff@mtu.edu"
+# ).first_or_create(
+#   password: "password",
+#   first_name: "Jill",
+#   last_name: "Doe",
+#   member_since: Time.now,
+#   user_type: "Staff"
+# )
+#
+# member = User.where(
+#   email: "member@mtu.edu"
+# ).first_or_create(
+#   password: "password",
+#   first_name: "Jack",
+#   last_name: "William",
+#   member_since: Time.now,
+#   user_type: "Member"
+# )
 (1...5).each do |day|
   (15...21).each do |time|
     time2 = time + 1
