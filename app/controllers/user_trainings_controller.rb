cdd79752eaa74b81a02c83f390d2933722ceccef
jjwyrzyk <jjwyrzyk@mtu.edu>
2019-01-25 16:31:04 -0500

    Make user trainings use models

--- app/controllers/user_trainings_controller.rb
+++ app/controllers/user_trainings_controller.rb
@@ -1,11 +1,14 @@
 class UserTrainingsController < ApplicationController
   load_and_authorize_resource
+  respond_to :html, :json
 
   def new
 
     @user_trainings = Training.all
     @user_training = UserTraining.new
     @user = User.find(params[:user_id])
+    session[:return_to] ||= request.referer
+    respond_modal_with @user_training
 
   end
 
@@ -14,7 +17,7 @@ class UserTrainingsController < ApplicationController
     training = Training.find(user_training_params[:training_id])
 
     if UserTraining.exists?(user: user, training: training)
-      redirect_to new_user_user_training_path(user), notice: 'User training already exists.'
+      redirect_to session.delete(:return_to), notice: 'User training already exists.'
       return
     end
 
@@ -24,7 +27,7 @@ class UserTrainingsController < ApplicationController
     @user_training.created_by = current_user
 
     if @user_training.save
-      redirect_to new_user_user_training_path(user), notice: "User trained on #{training.name}."
+      redirect_to session.delete(:return_to), notice: "User trained on #{training.name}."
     else
       render 'new'
     end
