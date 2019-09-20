8a55627a4c2649f990f122d88aefb6dd425c8822
jjwyrzyk <jjwyrzyk@mtu.edu>
2019-09-20 16:48:14 -0400

    Ability to add multiple trainings at one time

--- app/controllers/user_trainings_controller.rb
+++ app/controllers/user_trainings_controller.rb
@@ -14,20 +14,31 @@ class UserTrainingsController < ApplicationController
 
   def create
     user = User.find(params[:user_id])
-    training = Training.find(user_training_params[:training_id])
+    trainings = Training.where(:id => params[:training_ids])
 
-    if UserTraining.exists?(user: user, training: training)
-      redirect_to session.delete(:return_to), notice: 'User training already exists.'
-      return
+    successfulTrainings = []
+    success = true
+    trainings.each do |training|
+      # If user already has this training, skip adding it, move onto next in list
+      next if UserTraining.exists?(user: user, training: training)
+
+      # Otherwise, create the user training and save it
+      @user_training = UserTraining.new
+      @user_training.training = training
+      @user_training.user = user
+      @user_training.created_by = current_user
+      saveStatus = @user_training.save
+
+      if saveStatus
+        successfulTrainings.push(training.name)
+      else
+        success = false
+      end
+
     end
 
-    @user_training = UserTraining.new
-    @user_training.training = training
-    @user_training.user = user
-    @user_training.created_by = current_user
-
-    if @user_training.save
-      redirect_to session.delete(:return_to), notice: "User trained on #{training.name}."
+    if success
+      redirect_to session.delete(:return_to), notice: "User trained successfully on: #{successfulTrainings.join(", ")}"
     else
       render 'new'
     end
@@ -36,12 +47,21 @@ class UserTrainingsController < ApplicationController
   def destroy
       redirect_to users_path, notice: 'Wrong Method.'
   end
-
-
-
-private
-
-  def user_training_params
-    params.require(:user_training).permit(:training_id)
-  end
+  #
+  # if UserTraining.exists?(user: user, training: training)
+  #   redirect_to session.delete(:return_to), notice: 'User training already exists.'
+  #   return
+  # end
+  #
+  # @user_training = UserTraining.new
+  # @user_training.training = training
+  # @user_training.user = user
+  # @user_training.created_by = current_user
+  #
+  #
+  # if @user_training.save
+  #   redirect_to session.delete(:return_to), notice: "User trained on #{training.name}."
+  # else
+  #   render 'new'
+  # end
 end
